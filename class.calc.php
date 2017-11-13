9dcca74677883b03d97dbaf499c9484e7c292e9b
Dev3211 <devsarma321@gmail.com>
2017-11-13 12:34:50 +0530

    Create class.calc.php

---
+++ class.calc.php
@@ -0,0 +1,18 @@
+<?php
+class Calc
+
+{
+	 
+ function add($a, $b){
+ $result = array();
+ $result = $a + $b;
+ return $result;
+ }
+
+ function sub($a, $b){
+ $result = array();
+ $result = $a - $b;
+ return $result;
+ }
+ 
+}
